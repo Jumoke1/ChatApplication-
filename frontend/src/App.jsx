b7ea4c3f5c000d5c20e432b6035d38169da412ec
Jumoke1 <kazeemjumoke12@gmail.com>
2026-07-17 15:54:40 +0100

    modified app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/Dashboard';
 import TopNavbar from "./components/TopNavbar";
-import SideNavbar from './components/Sidebar/SideNavbar'
+import SideNavbar from './components/Sidebar/SideNavbar';
 import ProfileSidebar from "./components/Sidebar/ProfileSidebar";
-import MiddleChatarea  from "./components/Sidebar/MiddleChatarea"
-
+import MiddleChatarea from "./components/Sidebar/MiddleChatarea";
 
 function App() {
-  return(
-   
-    <Router>
-      <div className="App">
-        <Routes>
-              {/* by default users are redirected to login */}
-              <Route path='/' element={<Navigate to='/login' replace/>}/>
-
-              {/*Authentication routes*/}
-              <Route path='/login' element={<Login/>}/>
-              <Route path="/register" element= {<Register/>}/>
-
-
-               {/* Main Dashboard */}
-               <Route path="/dashboard" element= {<Dashboard/>}/>
-               <Route path="/topnavbar" element= {<TopNavbar/>}/>
-               <Route path="/sidenavbar" element={<SideNavbar/>}/>
-               <Route path="/profilesidebar" element={<ProfileSidebar/>}/>
-               <Route path="/middlechatarea" element={<MiddleChatarea/>}/>
-
-               {/*fallback routes for unknown routes will redirect to login */}
-               <Route path="*" element={<Navigate to="/login replace"/>}/>
-          </Routes>
-      </div>
-    </Router>
-
-  )
+  return (
+    <div className="App">   // ← Keep the div, remove Router
+      <Routes>
+        <Route path='/' element={<Navigate to='/login' replace />} />
+        <Route path='/login' element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/topnavbar" element={<TopNavbar />} />
+        <Route path="/sidenavbar" element={<SideNavbar />} />
+        <Route path="/profilesidebar" element={<ProfileSidebar />} />
+        <Route path="/middlechatarea" element={<MiddleChatarea />} />
+     
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
