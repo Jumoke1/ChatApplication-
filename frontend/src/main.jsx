734a0c5c6128b41b215621b89f86fc70fa4c670f
Jumoke1 <kazeemjumoke12@gmail.com>
2026-07-17 14:32:45 +0100

    modified main.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,20 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css'
-
+import './index.css';
 import { SocketProvider } from "./context/SocketContext";
 import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-    <SocketProvider>     
-      <AuthProvider>      
-        <App />
-      </AuthProvider>
-    </SocketProvider>
+    <BrowserRouter>          
+      <SocketProvider>        
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </SocketProvider>
+    </BrowserRouter>
   //</React.StrictMode>
 );
